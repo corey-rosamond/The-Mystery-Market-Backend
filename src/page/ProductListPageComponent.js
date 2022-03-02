16cb7d5a84d944f6447506db4b94c6e7e1b54a4b
corey-rosamond <97810442+corey-rosamond@users.noreply.github.com>
2022-03-02 11:56:14 -0800

    Working on setting up a datagrid

--- src/page/ProductListPageComponent.js
+++ src/page/ProductListPageComponent.js
@@ -1,8 +1,18 @@
 import React from "react";
 import styled from "styled-components";
 import {
+  DataGrid
+} from "@material-ui/data-grid";
+import {
+  DeleteOutline
+} from "@material-ui/icons";
+import {
   connect
 } from "react-redux";
+import {
+  Link
+} from "react-router-dom";
+
 import {
   getProductsStart,
   getProductsSuccess,
@@ -15,6 +25,25 @@ import {
 const Container = styled.div`
   flex: 7;
   padding: 10px 20px 10px 20px;
+`;
+
+const List = styled.div``;
+
+const ListItem = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Image = styled.img`
+
+`;
+
+const EditButton = styled.button`
+
+`;
+
+const DeleteButton = styled.button`
+
 `;
 
 class ProductListPageComponent extends React.Component
@@ -40,9 +69,57 @@ class ProductListPageComponent extends React.Component
 
   render()
   {
+    let products = this.props.product.products;
+    let columns = [
+      { field: "_id", headerName: "ID", width: 220 },
+      {
+        field: "product",
+        headerName: "Product",
+        width: 200,
+        renderCell: (params) => {
+          return (
+            <ListItem>
+              <Image src={params.row.img} alt="" />
+              {params.row.title}
+            </ListItem>
+          );
+        },
+      },
+      { field: "inStock", headerName: "Stock", width: 200 },
+      {
+        field: "price",
+        headerName: "Price",
+        width: 160,
+      },
+      {
+        field: "action",
+        headerName: "Action",
+        width: 150,
+        renderCell: (params) => {
+          return (
+            <>
+              <Link to={"/product/" + params.row._id}>
+                <EditButton>Edit</EditButton>
+              </Link>
+              <DeleteOutline
+              />
+            </>
+          );
+        },
+      },
+    ];
     return (
       <Container>
-        Product List
+        <List>
+          <DataGrid
+            rows={products}
+            disableSelectionOnClick
+            columns={columns}
+            getRowId={(row) => row._id}
+            pageSize={8}
+            checkboxSelection
+          />
+        </List>
       </Container>
     );
   }
